2c2b529f0f4b2322d8495f61da817c17522e996a
Alice <maiora.alice@gmail.com>
2025-11-22 12:17:20 +0800

    NavBar.jsx, music sheet icon

---
+++ jurassic-spark_frontend/src/components/NavBar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+
+// Import logo and music sheet(hamburger) icon
+import Logo from '../img/logo.png';      // mobile logo 
+import NoteIcon from '../img/Note.png';  // hamburger icon 
+
+const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+      {/* Mobile Logo */}
+      <div className="navbar-logo">
+        <img src={Logo} alt="VibeLab Logo" className="logo-img" />
+      </div>
+
+      {/* Desktop Title */}
+      <div className="navbar-title">
+        VibeLab
+      </div>
+
+      {/* Hamburger for mobile */}
+      <button
+        className="navbar-hamburger"
+        onClick={() => setMenuOpen(!menuOpen)}
+        aria-label="Open menu"
+      >
+        <img src={NoteIcon} alt="Menu" className="hamburger-img" />
+      </button>
+
+      {/* Nav links */}
+      <ul className={`navbar-links ${menuOpen ? 'open' : ''}`}>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/register">Sign Up</Link></li>
+        <li><Link to="/playlists">Create a Playlist</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
