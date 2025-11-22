d3454070923e746c174c610ba5ffaa4eb160d6fd
Alice <maiora.alice@gmail.com>
2025-11-23 00:30:29 +0800

    Navbar, linked login and homepage

--- jurassic-spark_frontend/src/App.jsx
+++ jurassic-spark_frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 import './Master.css';
 
 function App() {
   return (
     <Router>
       <NavBar />
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        {/* Add other routes here */}
+      </Routes>
     </Router>
   );
 }
--- jurassic-spark_frontend/src/components/NavBar.jsx
+++ jurassic-spark_frontend/src/components/NavBar.jsx
@@ -2,22 +2,20 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 
-import Logo from '../assets/VibeLab_mobile.png';      // first logo (mobile)
-import NoteIcon from '../assets/Note.png';            // hamburger icon
+import Logo from '../assets/VibeLab_mobile.png';
+import NoteIcon from '../assets/Note.png';
 
 const NavBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <nav className="navbar">
-      {/* Mobile Logo */}
-      <div className="navbar-logo">
-        <img src={Logo} alt="VibeLab Logo" className="logo-img" />
-      </div>
-
-      {/* Desktop Title */}
-      <div className="navbar-title">
-        VibeLab
+      {/* Logo and Title together */}
+      <div className="navbar-brand">
+        <Link to="/">
+            <img src={Logo} alt="VibeLab Logo" className="logo-img" />
+        </Link>
+        <span className="navbar-title">VibeLab</span>
       </div>
 
       {/* Hamburger for mobile */}
@@ -31,9 +29,9 @@ const NavBar = () => {
 
       {/* Nav links */}
       <ul className={`navbar-links ${menuOpen ? 'open' : ''}`}>
+        <li><Link to="/playlists">Create a Playlist</Link></li>
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/register">Sign Up</Link></li>
-        <li><Link to="/playlists">Create a Playlist</Link></li>
       </ul>
     </nav>
   );
